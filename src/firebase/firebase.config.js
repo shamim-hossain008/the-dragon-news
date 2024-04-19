a3a92e9e790d9e1b81d419caddd149405ab05a91
shamim-hossain008 <shamim.hossain3573@gmail.com>
2024-04-19 19:23:37 +0800

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCs3RzUAxX-CVtlhGrtUq-ppcBbbH8JYk4",
+  authDomain: "the-dragon-news-b1b24.firebaseapp.com",
+  projectId: "the-dragon-news-b1b24",
+  storageBucket: "the-dragon-news-b1b24.appspot.com",
+  messagingSenderId: "363982616664",
+  appId: "1:363982616664:web:40cef3c6486383d2bdf22d",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import Header from "../Shared/Header/Header";
+import Navbar from "../Shared/Navbar/Navbar";
+
+const Home = () => {
+  return (
+    <div>
+      <Header></Header>
+      <Navbar></Navbar>
+    </div>
+  );
+};
+
+export default Home;
