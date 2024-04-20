238097a150088ad26433fbec8772f45f8b792e8d
shamim-hossain008 <shamim.hossain3573@gmail.com>
2024-04-20 15:58:40 +0800

    login page added

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../Shared/Navbar/Navbar";
+
+const Login = () => {
+  return (
+    <div>
+      <Navbar />
+      <h2>This is log in page</h2>
+    </div>
+  );
+};
+
+export default Login;
